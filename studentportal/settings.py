80b1f0ce9f129b01455b321541a478fbd92fc461
nithin DS <nithinsuresh0727@gmail.com>
2026-05-20 12:09:24 +0530

    Fixed ALLOWED_HOSTS for Render

--- studentportal/settings.py
+++ studentportal/settings.py
@@ -6,7 +6,11 @@ SECRET_KEY = 'django-insecure-12345-change-me'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'studentportal-uamg.onrender.com'
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
