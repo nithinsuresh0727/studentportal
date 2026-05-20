f967384ce4241e24b2d6d396e993f88824950b95
nithin DS <nithinsuresh0727@gmail.com>
2026-05-20 11:42:04 +0530

    Fixed register_view import error

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,20 +1,9 @@
 from django.urls import path
-
-from .views import (
-    register_view,
-    login_view,
-    dashboard_view,
-    logout_view
-)
+from .views import login_view, dashboard_view, logout_view, register
 
 urlpatterns = [
-
     path('', login_view, name='login'),
-
-    path('register/', register_view, name='register'),
-
+    path('register/', register, name='register'),
     path('dashboard/', dashboard_view, name='dashboard'),
-
     path('logout/', logout_view, name='logout'),
-
 ]
