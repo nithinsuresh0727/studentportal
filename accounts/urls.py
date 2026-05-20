e08309a70dbe63555846537901ebf8ac7648623f
nithin DS <nithinsuresh0727@gmail.com>
2026-05-20 11:46:06 +0530

    Fixed Django 500 error and views/urls issues

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import login_view, dashboard_view, logout_view, register
+from .views import login_view, register_view, dashboard_view, logout_view
 
 urlpatterns = [
     path('', login_view, name='login'),
-    path('register/', register, name='register'),
+    path('register/', register_view, name='register'),
     path('dashboard/', dashboard_view, name='dashboard'),
     path('logout/', logout_view, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 def login_view(request):
+    return render(request, "login.html")
 
-    if request.method == "POST":
+def register_view(request):
+    return render(request, "register.html")
 
-        username = request.POST.get("username")
-        password = request.POST.get("password")
+def dashboard_view(request):
+    return render(request, "dashboard.html")
 
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect("dashboard")
-
-    return render(request, "accounts/login.html")
+def logout_view(request):
+    logout(request)
+    return redirect('login')
