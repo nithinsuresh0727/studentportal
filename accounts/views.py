efef1545ab8a5562105cbf18d4c4c638610c9ff7
nithin DS <nithinsuresh0727@gmail.com>
2026-05-20 11:35:17 +0530

    simplified login system username password only

--- accounts/views.py
+++ accounts/views.py
@@ -1,92 +1,17 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponse
-import csv
+from django.contrib.auth import authenticate, login
 
-
-# REGISTER
-def register_view(request):
-
-    if request.method == 'POST':
-
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-
-        User.objects.create_user(
-            username=username,
-            email=email,
-            password=password
-        )
-
-        return redirect('login')
-
-    return render(request, 'accounts/register.html')
-
-
-# LOGIN
 def login_view(request):
 
-    if request.method == 'POST':
+    if request.method == "POST":
 
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
+        username = request.POST.get("username")
+        password = request.POST.get("password")
 
-        try:
+        user = authenticate(request, username=username, password=password)
 
-            user_obj = User.objects.get(
-                username=username,
-                email=email
-            )
+        if user is not None:
+            login(request, user)
+            return redirect("dashboard")
 
-            user = authenticate(
-                request,
-                username=username,
-                password=password
-            )
-
-            if user is not None:
-
-                login(request, user)
-
-                return redirect('dashboard')
-
-        except User.DoesNotExist:
-            pass
-
-    return render(request, 'accounts/login.html')
-
-
-# DASHBOARD
-def dashboard_view(request):
-
-    if request.method == 'POST':
-
-        student_name = request.POST.get('student_name')
-
-        response = HttpResponse(content_type='text/csv')
-
-        response['Content-Disposition'] = 'attachment; filename="student.csv"'
-
-        writer = csv.writer(response)
-
-        writer.writerow(['Student Name', 'User Email'])
-
-        writer.writerow([
-            student_name,
-            request.user.email
-        ])
-
-        return response
-
-    return render(request, 'accounts/dashboard.html')
-
-
-# LOGOUT
-def logout_view(request):
-
-    logout(request)
-
-    return redirect('login')
+    return render(request, "accounts/login.html")
